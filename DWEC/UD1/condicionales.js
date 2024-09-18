a6188a316a77c8e01981f866c9d655d39ab4d519
jesusO <jesusdortiz18@gmail.com>
2024-09-18 14:06:18 +0200

    upload(relacion 5 hasta los swtich)

---
+++ DWEC/UD1/condicionales.js
@@ -0,0 +1,18 @@
+let valor = 3;
+let valor2 = -2;
+ 
+document.write((valor > 0) ? 'Es mayor a 0' : 'Es menor que 0');
+document.write('<br />')
+
+// una cadena vacia siempre es falso 
+let vacio = "";
+document.write(( !vacio ) ? 'false' : 'true');
+
+/*
+    equal == 
+    not equal != 
+    strict equal === got to be same type 
+    stict not equal !==
+    operacion exponencial **  // 2 ** 3 = 8 
+*/
+
