4fcdeae88c0ae81d26f1668ae054feb7cd547c94
jesusO <jesusdortiz18@gmail.com>
2024-09-18 12:01:22 +0200

    upload(relacion II ej 4 y 5)

---
+++ DWEC/UD1/relacionIIEj4.js
@@ -0,0 +1,15 @@
+var celsius = parseFloat((prompt('Indique los Celsius: ')));
+var celToFa = (1.8 * celsius) + 32;
+
+document.write(celsius + 'ºC is' +  celToFa + 'ºF');
+document.write('<br/>');
+
+
+var fahrenheit = parseFloat((prompt('Indique los Fahrenheit: ')));
+var faToCel = ((fahrenheit - 32) / 1.8);
+
+document.write(fahrenheit + 'ºF is' +  faToCel + 'ºC');
+document.write('<br/>');
+
+// f = (9/5 * c) - 32 
+// c = (5/9)  * (f - 32)
