755b461874be8439b7f74a6d36e7d2213f903771
jesusO <jesusdortiz18@gmail.com>
2024-09-23 12:36:56 +0200

    upload(finalizacion ejercicios js)

--- DWEC/UD1/repasoFinalBucle.js
+++ DWEC/UD1/repasoFinalBucle.js
@@ -1,13 +1,3 @@
-/**
-
-Realizar la suma de todos los números pares entre N y M donde N y M los ingresa un usuario.
-
-Realizar la sumatoria de los primeros N números, donde N es ingresado por el usuario.
-
-Realizar el factorial de los primeros N números.
-*/
-
-
 
 // 1 
 // let num = parseInt(prompt('Ingrese un numero'));
@@ -37,14 +27,57 @@ Realizar el factorial de los primeros N números.
 // }
 
 // 5 Pedir al usuario que ingrese un número en un prompt, hacer la suma de todos los dígitos
+// let palabra = prompt('Ingrese un numero');
+// let suma = 0;
 
-let num;
-let suma = 0;
- 
-let palabra = prompt('Ingrese un numero');
+// for (let i = 0; i < palabra.length; i++) {
+//     suma += parseInt(palabra[i], 10);    
+// }
 
-for (let i = 0; i < palabra.length; i++) {
-    document.write(palabra.charAt[0]);
-}
+// if ((palabra.length  < 1 ) ? document.write(palabra) : document.write(suma));
+
+// //6 Realizar la suma de todos los números pares entre N y M donde N y M los ingresa un usuario.
+// let n = 0;
+// let m = 0;
+// let suma = 0;
+// let mayor; 
+
+// while(n == m){
+//     n = parseInt(prompt('Ingrese un numero'));
+//     m = prompt(('Ingrese otro numero'));
+// }
+
+// if (( m > n) ? mayor = m : mayor = n);
+
+// for ( let i = n; i <= m; i++){
+//     if (i%2 == 0){
+//         suma += i;
+//     }
+// }
+
+// document.write(suma);
 
 
+// // 7 Realizar la sumatoria de los primeros N números, donde N es ingresado por el usuario.
+// let n = 0;
+// let suma = 0;
+
+// n = parseInt(prompt('Ingrese un numero'));
+
+// for ( let i = 1; i <= n; i++){
+//     suma += i;
+// }
+
+// document.write(suma);
+
+// 8 Realizar el factorial de los primeros N números.
+// factorial n! = n * (n-1) ... 2 
+
+// let n = parseInt(prompt('Ingresa un numero'));
+// let f = 1;
+
+// for ( let i = 1; i <= n; i++){
+//     f *= i;
+// };
+
+// document.write(f);   
