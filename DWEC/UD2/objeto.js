5102a8d6d1d77307652b65b02c71fe9f4246cb43
jesusO <jesusdortiz18@gmail.com>
2024-09-24 13:16:31 +0200

    upload(finalizacion de la relacion funciones Math)

---
+++ DWEC/UD2/objeto.js
@@ -0,0 +1,24 @@
+//objeto fecha 
+let fecha = new Date();
+document.write(fecha);
+
+let cadena = new String();
+cadena = 'hola mundo';
+
+document.write('<br />' + cadena.length)
+
+// objetos
+let miCoche = {
+    marca: "Ford",
+    modelo: "Focus"
+};
+
+miCoche.marca; miCoche.modelo;
+
+// clase
+const Coche = {
+    property1: value1, // property name may be an identifier
+    2: value2, // or a number
+    "property n": value3, // or a string
+}
+
---
+++ DWEC/UD2/relacion.js
@@ -0,0 +1,52 @@
+// wsheet 2 ( 1 5 7 8 )
+// 1
+// let random0y1 = Math.random() * 1;
+// document.write(random0y1 + "<br />");
+
+// let random100y200 = Math.floor(Math.random() * (200 - 100) + 100);
+// document.write(random100y200 + "<br />");
+
+// 5
+// let a = parseInt(prompt('Coeficiente 1'));
+// let b = parseInt(prompt('Coeficiente 2'));
+// let c = parseInt(prompt('Coeficiente 3'));
+
+// let xP =( (-b) + ( Math.sqrt(( b * b ) - (4 * a * c)) ) ) / ( 2 * a );
+// let xN =( (-b) - ( Math.sqrt(( b * b ) - (4 * a * c)) ) ) / ( 2 * a );
+
+// document.write(xP + ", " + xN);
+
+// 7 
+// var col = 2;
+// var fil = 10;
+// var alt = 50;
+// var anc = 50;
+
+// document.write('<table border="0" cellspacing="2" bgcolor="black" width="200" >');
+
+// for ( let i = 1; i <= fil; i++){
+//     document.write('<tr bgcolor="white" height="' + alt +  '" >');
+
+//     document.write('<td width="' + anc + '">' + i + '</td>');
+//     document.write('<td width="' + anc + '">' + Math.sin(i) + '</td>');
+    
+// }
+
+// document.write('</tr>');
+// document.write('</table>');
+
+
+// 8
+// https://thypix.com/wp-content/uploads/horse-113.jpg
+// https://th.bing.com/th/id/OIP.qN4dEf-pKeaKa698g6MpOgHaEK?rs=1&pid=ImgDetMain
+// https://www.hdwallpapers.in/download/brown_horse_with_background_of_sunset_and_clouds_hd_horse-HD.jpg
+
+let i1 = "https://thypix.com/wp-content/uploads/horse-113.jpg"; 
+let i2 = "https://mui.today/__export/1602378684955/sites/mui/img/2020/10/10/58.jpg_693687776.jpg"; 
+let i3 = "https://www.hdwallpapers.in/download/brown_horse_with_background_of_sunset_and_clouds_hd_horse-HD.jpg"; 
+
+let imagenes = [i1, i2, i3];
+
+let r = Math.trunc(Math.random() * 3 + 0);
+
+document.write('<img src="'+ imagenes[r] +'" width=700 height="400"');
