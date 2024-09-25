c2a29bc21fab9a903685b2505f3775d67ca92a6e
jesusO <jesusdortiz18@gmail.com>
2024-09-25 12:52:06 +0200

    upload (ejercicio contador js)

--- DWEC/UD3/relI.js
+++ DWEC/UD3/relI.js
@@ -11,18 +11,43 @@
 
 
 // 2
-var f = new Date();
-const agregarDia = 24 * 60 * 60 * 1000;
-const agregarAños = 365 * 24 * 60 * 60 * 1000;
+// var f = new Date();
+// const agregarDia = 24 * 60 * 60 * 1000;
+// const agregarAños = 365 * 24 * 60 * 60 * 1000;
 
-var fechaHoy = f.getTime();
-var fecha85 = new Date (fechaHoy + (agregarDia * 85));
-var fecha187 = new Date(fechaHoy + (agregarDia * 187));
-var fecha85y2 = new Date(fecha85  + (agregarAños * 2));
-var fecha186 = new Date(fechaHoy - (agregarDia * 186));
+// var fechaHoy = f.getTime();
+// var fecha85 = new Date (fechaHoy + (agregarDia * 85));
+// var fecha187 = new Date(fechaHoy + (agregarDia * 187));
+// var fecha85y2 = new Date(fecha85  + (agregarAños * 2));
+// var fecha186 = new Date(fechaHoy - (agregarDia * 186));
+// var fechaResto = (fecha85 - fecha187);
 
-document.write("Fecha hoy: " + f.toLocaleDateString("en-GB") + "<br/>");
-document.write("Fecha en 85 días: " + fecha85.toLocaleDateString("en-GB") + "<br/>");
-document.write("Fecha en 187 días: " + fecha187.toLocaleDateString("en-GB") + "<br/>");
-document.write("Fecha en 2 años: " + fecha85y2.toLocaleDateString("en-GB") + "<br/>");
-document.write("Fecha hace 186 días: " + fecha186.toLocaleDateString("en-GB") + "<br/>");
+// document.write("Fecha hoy: " + f.toLocaleDateString("en-GB") + "<br/>");
+// document.write("Fecha en 85 días: " + fecha85.toLocaleDateString("en-GB") + "<br/>");
+// document.write("Fecha en 187 días: " + fecha187.toLocaleDateString("en-GB") + "<br/>");
+// document.write("Fecha en 2 años: " + fecha85y2.toLocaleDateString("en-GB") + "<br/>");
+// document.write("Fecha hace 186 días: " + fecha186.toLocaleDateString("en-GB") + "<br/>");
+// document.write("Variable resto: " + fechaResto + "<br/>");
+
+// // 3 
+// var detener = 1000;
+// var n = 60;
+
+// function contador(){
+//     document.write('<h1>' + n + '</h1>');
+//     n--;
+// }
+
+// setInterval(contador, detener);
+
+var n = 60;
+
+function contador() {
+    if (n >= 0) {
+        console.log(n);
+        n--;
+        setTimeout(contador, 1000);
+    }
+}
+
+contador();
