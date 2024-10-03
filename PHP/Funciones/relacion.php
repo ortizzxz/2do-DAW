818d2b6ecba76cd470dadc1670ca1c098cc68ed3
jesusO <jesusdortiz18@gmail.com>
2024-10-03 10:49:43 +0200

    upload(relacion php)

--- PHP/Funciones/relacion.php
+++ PHP/Funciones/relacion.php
@@ -64,7 +64,7 @@ function devuelveString($str){
     return $ext;
 }
 
-// echo devuelveString($_GET['st']);
+echo devuelveString($_GET['st']);
 
 // 4 Potencias 
 /*Escribe una función para calcular potencias. La función recibirá como argumentos la base y el exponente. 
@@ -73,12 +73,45 @@ El exponente es opcional y tiene por defecto el valor 2 */
 function potencias($base, $exponente = 2){
     $valor = 0;
 
-    for ($i = 0; $i < $exponente; $i++){
-        $valor += $base * $base;
+    if($exponente === 1 || $exponente === 0){
+        return 1;
+    } 
+
+    for ($i = 1; $i < $exponente; $i++){
+        if($valor === 0){
+            $valor += $base * $base;
+        }else{
+            $valor = $valor * $base;
+            
+        }
     }
 
     return $valor;
 }
 
-echo potencias(2, 5);
+// echo potencias(2, 0);
+
+// function fechaHoy(){
+//     date_default_timezone_set('Europe/Madrid');
+//     return  date('d-m-Y');
+// }
+
+// function fechaEspanol($fecha) {
+//     $fecha = substr($fecha, 0, 10);
+//     $numeroDia = date('d', strtotime($fecha));
+//     $dia = date('l', strtotime($fecha));
+//     $mes = date('F', strtotime($fecha));
+//     $anio = date('Y', strtotime($fecha));
+//     $dias_ES = array("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
+//     $dias_EN = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+//     $nombredia = str_replace($dias_EN, $dias_ES, $dia);
+//     $meses_ES = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+//     $meses_EN = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+//     $nombreMes = str_replace($meses_EN, $meses_ES, $mes);
+//     return $nombredia." ".$numeroDia." de ".$nombreMes." de ".$anio;
+//   }
+
+// echo fechaEspanol(fechaHoy());
+
+
 ?>
