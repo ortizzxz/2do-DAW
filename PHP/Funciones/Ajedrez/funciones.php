1abfecb6cb3a7bb8edc3e638559261569a63d2aa
jesusO <jesusdortiz18@gmail.com>
2024-10-03 14:14:36 +0200

    upload ajedrez php

--- PHP/Funciones/Ajedrez/funciones.php
+++ PHP/Funciones/Ajedrez/funciones.php
@@ -2,48 +2,36 @@
 /* Funcion para pintar una tabla de ajedrez */
 function creaTable(){ 
     
-    $piezasBlancas=array("torreb","caballob","alfilb","reinab","reyb","alfilb","caballob","torreb");
-    $piezasNegras=array("torren","caballon","alfiln","reinan","reyn","alfiln","caballon","torren");
+    $piezasBlancas = array("torreb","caballob","alfilb","reinab","reyb","alfilb","caballob","torreb");
+    $piezasNegras = array("torren","caballon","alfiln","reinan","reyn","alfiln","caballon","torren");
     
     for($i = 0; $i < 8; $i++){
+        // bucle inicial para crear los tr donde $i => fila 
         echo '<tr>';
 
+        // bucle indexado donde $a => columna
         for($a = 0; $a < 8; $a++){
-            if($i%2==0){
-                if($i == 6){
-                    if($a%2==0){
-                        echo '<td class="blanca"><img src="./fichasAjedrez/peon-negro.png"></td>';
-                    }else {
-                        echo '<td class="gris"><img src="./fichasAjedrez/peon-negro.png"></td>';
-                    } 
-                }else{
-                    if($a%2==0){
-                        echo '<td class="blanca">&nbsp;</td>';
-                    }else {
-                        echo '<td class="gris">&nbsp;</td>';
-                    } 
-                }
-            }else{
-                if($i == 1){
-                    if($a%2==0){
-                        echo '<td class="gris"><img src="./fichasAjedrez/peon-negro.png"></td>';
-                    }else {
-                        echo '<td class="blanca"><img src="./fichasAjedrez/peon-negro.png"></td>';
-                    } 
-                }else{
-                    if($a%2==0){
-                        echo '<td class="gris">&nbsp;</td>';
-                    }else {
-                        echo '<td class="blanca">&nbsp;</td>';
-                    } 
-                }
+            $class = ($i + $a) % 2 == 0 ? 'blanca' : 'gris'; // nFila + nColummna / 2 => posicion 
+            
+            echo "<td class='$class'>";
 
+            if($i == 0){
+                echo "<img src='./fichasAjedrez/{$piezasNegras[$a]}.png'>";
+            } else if($i == 1){
+                echo "<img src='./fichasAjedrez/peon-negro.png'>";
+            } else if($i == 6){
+                echo "<img src='./fichasAjedrez/peon-blanco.png'>";
+            } else if($i == 7){
+                echo "<img src='./fichasAjedrez/{$piezasBlancas[$a]}.png'>";
+            } else {
+                echo '&nbsp;';
             }
+
+            echo '</td>';
         }
 
-        echo '<tr />';
+        echo '</tr>';
     }
     
 }
-
-?>
+?>  
