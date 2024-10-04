80afdeca675354e0a96484d0d01a92c32bb85be7
jesusO <jesusdortiz18@gmail.com>
2024-10-04 11:08:27 +0200

    upload (tabajando con funciones js)

---
+++ DWEC/UD4/index.js
@@ -0,0 +1,23 @@
+function imprimeConsola(error){
+    console.log(error);
+}
+
+function orden(a, b){
+
+    if(a < b){
+        return 1;
+    }else if(b < a){
+        return -1;
+    }else{
+        return 0;
+    }
+
+}
+
+function orden3(a, b, c){
+    
+
+}
+
+var resultado = orden3("mathias", "jose", "francisco");
+console.log(resultado); 
