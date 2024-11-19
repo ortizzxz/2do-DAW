28827132e41fd962a90eef87e2d8bef13bc8686f
jesusO <jesusdortiz18@gmail.com>
2024-11-19 19:45:13 +0100

    upload

--- DWEC/UD10 SVG/index.js
+++ DWEC/UD10 SVG/index.js
@@ -3,66 +3,64 @@ window.onload = () => {
     let pelotas = new Array();
     let svgPadre = document.getElementById("svgPadre");
 
-    for (let index = 0; index < 10; index++) {
+    for (let index = 0; index < 1; index++) {
         pelotas.push(new Bola(svgPadre));
     }
 
-    for (const p of pelotas) {
-        setInterval(() => {
+    setInterval(() => {
+        for(let p of pelotas){
             p.mover();
-        }, 3);
-    }
+        }
+    }, 16);
         
 }
 
 class Bola {
     constructor(svgPadre) {
-        this.posicionX = 50;
-        this.posicionY = 50;
-        this.limiteX = 1000;
-        this.limiteY = 500;
+        this.posicionX = getRandomArbitrary(1, 1300);
+        this.posicionY = getRandomArbitrary(1, 500);
+        this.limiteX = 1340;
+        this.limiteY = 580;
         this.radio = 5;
-        this.velocidadX = 5;
-        this.velocidadY = 5;
+        this.velocidadX = (Math.random() - 0.5) * 10; 
+        this.velocidadY = (Math.random() - 0.5) * 10; 
         this.stroke = 'green';
         this.fill = 'blue';
-        this.createTag = svgPadre; 
+        this.svgPadre = svgPadre;
+        this.creaTag();
     }
 
-    creaTag(svgPagre) {
-        newPelota = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
-        newPelota.setAttribute('cx', this.posicionX);
-        newPelota.setAttribute('cy', this.posicionY);
-        newPelota.setAttribute('r', this.radio);
-        newPelota.setAttribute('stroke', this.stroke);
-        newPelota.setAttribute('stroke-width', '1');
-        newPelota.setAttribute('fill', this.fill);
-        document.getElementById(svgPagre).appendChild(this.newPelota);
-
+    creaTag() {
+        this.newPelota = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+        this.newPelota.setAttribute('cx', this.posicionX);
+        this.newPelota.setAttribute('cy', this.posicionY);
+        this.newPelota.setAttribute('r', this.radio);
+        this.newPelota.setAttribute('stroke', this.stroke);
+        this.newPelota.setAttribute('stroke-width', '1');
+        this.newPelota.setAttribute('fill', this.fill);
+        this.svgPadre.appendChild(this.newPelota);
     }
 
     mover() {
         this.posicionX += this.velocidadX;
         this.posicionY += this.velocidadY;
 
-        this.newPelota.setAttribute('cx', posicionX++);
-        this.newPelota.setAttribute('cy', posicionY++);
+        this.newPelota.setAttribute('cx', this.posicionX);
+        this.newPelota.setAttribute('cy', this.posicionY);
 
-        if ((this.posicionX + this.radio) > this.limiteX) {
+        if ((this.posicionX + this.radio) > this.limiteX || (this.posicionX - this.radio) < 0) {
             this.velocidadX *= -1;
-        } else {
-            if ((this.posicionX - this.radio) < 0) {
-                this.velocidadX *= -1;
-            }
         }
-
-        if ((this.posicionY + this.radio) > this.limiteY) {
+        
+        if ((this.posicionY + this.radio) > this.limiteY || (this.posicionY - this.radio) < 0) {
             this.velocidadY *= -1;
-        } else {
-            if ((this.posicionY - this.radio) < 0) {
-                this.velocidadY *= -1;
-            }
         }
+        
     }
 }
 
+
+// copiada de mdn
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
